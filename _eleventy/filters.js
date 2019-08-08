cb804412205e2cae160da3db2c007e28943c950c
Andrey Butenko <ghostmancer@gmail.com>
2019-08-08 11:30:26 -0700

    Create project page template

--- _eleventy/filters.js
+++ _eleventy/filters.js
@@ -1,3 +1,5 @@
 module.exports = {
-  getPreview: postContent => postContent.split('<p>///</p>')[0]
+  getPreview: postContent => postContent.split('<p>///</p>')[0],
+  getAntiPreview: postContent => postContent.split('<p>///</p>')[1] || '',
+  hasContent: str => str.length > 0
 };
